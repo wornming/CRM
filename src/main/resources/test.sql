95d5bdf1fe39eb373d1434b4932d7c6d40ae741a
wornming <wornming@gmail.com>
2016-08-20 19:59:23 +0800

    增加mapper

--- src/main/resources/test.sql
+++ src/main/resources/test.sql
@@ -2,3 +2,8 @@ select uname,userage,tiaomu from userinfo
 inner join datadirectorys on
 userinfo.role=datadirectorys.id
 where uname='a';
+
+
+select * from customer;
+
+
--- src/test/java/com/crm/test/Test.java
+++ src/test/java/com/crm/test/Test.java
@@ -3,13 +3,37 @@ package com.crm.test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.crm.bean.Chance;
+import com.crm.bean.Customer;
+import com.crm.bean.UserInfo;
+import com.crm.biz.ChanceBiz;
+import com.crm.biz.CustomerBiz;
+import com.crm.biz.UserInfoBiz;
+import com.crm.web.model.PageModel;
+
 import junit.framework.TestCase;
 
 public class Test extends TestCase {
 
 	public void test() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
-
-		System.out.println(context.getBean("sqlSession"));
+		UserInfo user =new UserInfo();
+		user.setUname("a");
+		user.setPwd("a");
+		UserInfoBiz biz=(UserInfoBiz) context.getBean("userInfoBizImpl");
+		System.out.println(biz.findUserInfo(user));
+	}
+	public void test2() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
+		CustomerBiz biz=(CustomerBiz) context.getBean("customerBizImpl");
+		PageModel<Customer> page=new PageModel<Customer>();
+		page.setPage(1);
+		System.out.println(biz.getCustomerBean(page));
+	}
+	public void test3() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
+		ChanceBiz biz=(ChanceBiz) context.getBean("chanceBizImpl");
+		PageModel<Chance> pagemodel=new PageModel<Chance>();
+		System.out.println(biz.FindChanceListPage(pagemodel));
 	}
 }
